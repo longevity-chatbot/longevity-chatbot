6531810e3e60d32b9aa97071038d57adac8efb34
fjonah <fjona_hysi@outlook.com>
2025-05-22 00:23:42 +0200

    Update document_store.py

--- longevity_rag/rag/document_store.py
+++ longevity_rag/rag/document_store.py
@@ -1,10 +1,15 @@
 
-from langchain.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma
 from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+
 from langchain.docstore.document import Document
 
 def create_vector_store(papers):
+    #generates embeddings (numerical representations) for each paper's summary using a pre-trained model (all-MiniLM-L6-v2)
     embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    #creates a list of Document objects, each containing a paper's summary and title. 
     docs = [Document(page_content=p["summary"], metadata={"title": p["title"]}) for p in papers]
+    # it stores these documents in a vector store (Chroma) for efficient retrieval based on similarity
     vectorstore = Chroma.from_documents(docs, embedding=embeddings)
     return vectorstore
